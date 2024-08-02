fd6bc070b87c73349fc2fcd89d45c198c208cfd7
Roy Anonuevo <roy.anonuevo@growthops.asia>
2024-08-02 13:49:53 +0800

    update

---
+++ app/(routes)/(homapge)/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage () {
+  return (
+    <main className='min-h-screen flex items-center justify-center py-10 bg-slate-500'>
+      <section className='w-full max-w-[600px] bg-white p-8 px-8 rounded-lg'>
+
+        <ul>
+          <li>Inputs</li>
+          <li>Dropdowns</li>
+        </ul>
+      </section>
+    </main>
+  )
+}
